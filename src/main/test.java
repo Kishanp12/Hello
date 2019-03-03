616d47fd3858a4654c5bc7ceaa8c840c360be890
Kishan Patel <Kishan@Kishans-MacBook.fios-router.home>
2019-03-02 23:25:55 -0500

    New File

--- src/main/test.java
+++ src/main/test.java
@@ -5,6 +5,44 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//add all numbers from 0-20 
+		// ****
+		// ***
+		// **
+		// *
+		
+		
+		
+		for(int row = 4; row > 0; row--) {
+			
+			
+			for(int col = 0; col < row; col++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+		
+		/* row | col
+		 * 4		0
+		 * 4		1
+		 * 4		2
+		 * 4		3
+		 * 4		4
+		 * 3		0
+		 * 3		1
+		 * 3		2
+		 * 3		3
+		 * 2		0
+		 * 2		1
+		 * 2		2
+		 */
+			
+		
+		
+		
+		
+		
 	}
 
 }
