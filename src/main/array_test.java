f6869221d5840818fbe70555f6026a7dc860d842
Mackensie Alvarez <greymattermack@gmail.com>
2019-03-06 17:34:37 -0500

    Test added

---
+++ src/main/array_test.java
@@ -0,0 +1,15 @@
+package main;
+
+public class array_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Make a problem to ask teacher N amount of students.
+		//Then for each student save their name and grade.
+		// Then tell me who has the highest grade
+		
+
+	}
+
+}
